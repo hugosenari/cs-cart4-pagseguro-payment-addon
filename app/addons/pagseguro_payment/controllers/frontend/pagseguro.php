74691b2efecb6afb9fe7d471582e717a5c2bb787
hugosenari <hugosenari@gmail.com>
2014-02-02 00:14:04 -0200

    adicionando debug

--- app/addons/pagseguro_payment/controllers/frontend/pagseguro.php
+++ app/addons/pagseguro_payment/controllers/frontend/pagseguro.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once('../../payments/helper.php');
-use Addons\PagSeguro\Helper as PSH;
-
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 // here comes the code for user payment success (this not mean that they pay)
@@ -14,7 +11,6 @@ if ($mode == 'complete')
     $cur_status = $order_short_info['status'];
     echo("asdkfhaljsdlhfalsdjf");
     print_r($order_short_info);
-    //PSH\logger( true), __FILE__, __LINE__);
     if($cur_status == 'N')
     {
         $pp_response = array();
